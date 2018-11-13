63959c79763699f51550f6e781eaccc8cf85a680
胡奇良 <hql@ipms.cn>
2018-11-13 11:40:42 +0800

    doc配置修改

---
+++ build/webpack.dist.doc.conf.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const webpackBaseConfig = require("./webpack.base.conf.js");
+const CompressionPlugin = require("compression-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+process.env.NODE_ENV = "production";
+
+module.exports = merge(webpackBaseConfig, {
+  // devtool: "source-map",
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "../docs/assets/dist"),
+    publicPath: "/dist",
+    filename: "index.min.js",
+    library: "gc_iview",
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
+  externals: {
+    vue: {
+      root: "Vue",
+      commonjs: "vue",
+      commonjs2: "vue",
+      amd: "vue"
+    },
+    iview: {
+      root: "iview",
+      commonjs: "iview",
+      commonjs2: "iview",
+      amd: "iview"
+    }
+  },
+  plugins: [
+    // @todo
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": '"production"'
+    }),
+    new UglifyJsPlugin({
+      parallel: true,
+      sourceMap: true
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|css)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
+});
