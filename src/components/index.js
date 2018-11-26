d29ab69df8542621cfb9d3f5de380cd16833b9ce
胡奇良 <hql@ipms.cn>
2018-11-26 15:41:17 +0800

    名字修改

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import AutoTable from "./AutoTable.vue";
 import FilterSearch from "./FilterSearch.vue";
 import TFSwitch from "./TFSwitch.vue";
-import DatePickers from "./DatePickers";
-import CurdLine from "./CurdLine";
+import DatePickers from "./DatePickers.vue";
+import CurdLine from "./CurdLine.vue";
 export default [AutoTable, FilterSearch, TFSwitch, DatePickers, CurdLine];
