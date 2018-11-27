3e2523f955953b3c4dc720f1f09a6563a54375e6
胡奇良 <hql@ipms.cn>
2018-11-27 10:52:00 +0800

    编译提交1.0.4

--- examples/main.js
+++ examples/main.js
@@ -1,11 +1,12 @@
 // import 'babel-polyfill'
 import Vue from "vue";
 import iview from "iview";
-import gc_iview from "../src/index";
+import gc_iview from "../dist/index.min.js";
 import App from "./app.vue";
 Vue.use(iview);
 // 使用gc_iview
 Vue.use(gc_iview);
+console.log(gc_iview);
 
 // 开启debug模式
 Vue.config.debug = true;
