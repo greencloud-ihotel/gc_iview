555b39238855442a604678c9ee9a662f17a666a9
xujian <xujian@ihotel.cn>
2019-05-27 11:15:52 +0800

    TFSwitch 更新至最新 修改props参数增加校验 提取字符串为变量

--- src/components/TFSwitch.vue
+++ src/components/TFSwitch.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <i-switch v-model='switchValue'
+              :disabled="disabled"
               @input='change'>
       <span slot="open">开</span>
       <span slot="close">关</span>
@@ -9,37 +10,65 @@
 </template>
 
 <script>
+const OPEN = "F";
+const CLOSE = "T";
+const SWITCH_ERROR = "switch-error";
+const SWITCH_SUCCESS = "switch-success";
 export default {
   name: "TFSwitch",
   data() {
     return {
-      switchValue: true
+      switchValue: true,
+      disabled: false
     };
   },
   props: {
-    row: Object,
-    url: String
+    row: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    updateUrl: {
+      type: String,
+      default: "",
+      required: true
+    },
+    method: {
+      type: String,
+      default: "put",
+      validator: val => {
+        const valid = ["get", "post", "delete", "put"];
+        return valid.indexOf(val.toLocaleLowerCase()) > -1;
+      }
+    }
   },
   created() {
-    this.switchValue = !!this.row && this.row["isHalt"] == "F";
+    if (this.updateUrl == "") {
+      this.disabled = true;
+      return false;
+    }
   },
   methods: {
     async change(value) {
-      this.row.isHalt = value ? "F" : "T";
-      this.row.updateIsHaltOnly = "T";
-      this.$http.put(this.url, this.row).then(resData => {
+      this.row.isHalt = value ? OPEN : CLOSE;
+      this.row.updateIsHaltOnly = CLOSE;
+
+      this.$http[this.method](this.updateUrl, this.row).then(resData => {
         if (resData.data.result !== 0) {
           this.switchValue = !this.switchValue;
-          this.row.isHalt = this.row.isHalt == "T" ? "T" : "F";
-          // this.$Message.error(resData.data.msg);
-          this.$emit("switch-error", this.row.isHalt);
+          this.row.isHalt = this.row.isHalt == CLOSE ? CLOSE : OPEN;
+          this.$emit(SWITCH_ERROR, this.row.isHalt);
         } else {
-          this.row.isHalt = this.row.isHalt == "F" ? "T" : "F";
+          this.row.isHalt = this.row.isHalt == OPEN ? CLOSE : OPEN;
           this.$Message.success(resData.data.msg);
-          this.$emit("switch-success", this.row.isHalt);
+          this.$emit(SWITCH_SUCCESS, this.row.isHalt);
         }
       });
     }
+  },
+  mounted() {
+    this.switchValue = !!this.row && this.row["isHalt"] == OPEN;
   }
 };
 </script>
