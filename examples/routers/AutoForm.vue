bc9f22337fa04e4560bec66c331a5786e12c78ca
胡奇良 <hql@ipms.cn>
2019-07-25 10:10:04 +0800

    Feat:AutoForm

--- examples/routers/AutoForm.vue
+++ examples/routers/AutoForm.vue
@@ -1,18 +1,93 @@
 <template>
-  <AutoForm v-model="autoForm.submitForm" :fields="autoForm.fields"></AutoForm>
+  <div class="contain">
+    {{ autoForm.submitForm }}
+    <AutoForm
+      v-model="autoForm.submitForm"
+      :fields="autoForm.fields"
+      :row="2"
+    ></AutoForm>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
+    const validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("Please enter your password"));
+      } else {
+        callback();
+      }
+    };
+    const validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("Please enter your password"));
+      } else {
+        callback();
+      }
+    };
     return {
       autoForm: {
-        submitForm: {},
-        fields: []
+        submitForm: {
+          name: "111",
+          name2: "@222"
+        },
+        fields: [
+          {
+            key: "name",
+            type: "input",
+            label: "审批人",
+            num: 2,
+            validators: [{ validator: validatePass }]
+          },
+          {
+            key: "a2.dd",
+            type: "input",
+            label: "aaa",
+            validators: [{ validator: validatePass2 }]
+          },
+          {
+            key: "a3.cc",
+            type: "input",
+            label: "审批人",
+            validators: [{ required: true, message: "必填3" }]
+          },
+          {
+            key: "name2",
+            type: "select",
+            label: "name2:",
+            options: [
+              {
+                label: "dddd",
+                value: "aaa"
+              },
+              {
+                label: "2d22",
+                value: "aaddda"
+              }
+            ]
+          },
+          {
+            key: "name3",
+            label: "name3",
+            render() {
+              return (
+                <div>
+                  <span>ddd</span>
+                  <span>ddd2</span>
+                </div>
+              );
+            }
+          }
+        ]
       }
     };
   }
 };
 </script>
 
-<style></style>
+<style>
+.contain {
+  width: 50%;
+}
+</style>
--- src/components/AutoForm.vue
+++ src/components/AutoForm.vue
@@ -1,14 +1,91 @@
-<template>
-  <div>ddd</div>
-</template>
-
 <script>
+import _ from "lodash";
 export default {
   name: "AutoForm",
+  props: {
+    value: {
+      //表单导出源数据
+      type: Object,
+      default() {
+        return {};
+      },
+      required: true
+    },
+    fields: {
+      //表单数据配置项
+      type: Array,
+      required: true
+    },
+    row: {
+      type: Number,
+      default() {
+        return 2;
+      }
+    }
+  },
+  computed: {
+    submitForm: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit("input", val);
+      }
+    }
+  },
   render() {
-    return <div>ddd</div>;
+    return (
+      <div class="autoForm">
+        {this.submitForm.name}
+        <Form
+          model={this.submitForm}
+          onInput={() => {}}
+          class="form"
+          // rules={rules}
+          labelWidth={70}
+          attrs={this.$attrs}
+          inline
+        >
+          {_.map(this.fields, val => {
+            const num = val.num ? val.num : 1;
+            return (
+              <FormItem
+                key={val.key}
+                prop={val.key}
+                label={val.label}
+                style={{ width: `${(100 * num) / this.row}%` }}
+                rules={val.validators ? val.validators : {}}
+              >
+                {val.key}
+                <Input
+                  type="text"
+                  placeholder={val.placeholder}
+                  value={this.value[val.key]}
+                  onInput={inputVal => {
+                    const newVal = _.set(this.submitForm, val.key, inputVal);
+                    this.submitForm = _.assign({}, newVal);
+                  }}
+                >
+                  <Icon type="ios-person-outline" slot="prepend"></Icon>
+                </Input>
+              </FormItem>
+            );
+          })}
+        </Form>
+      </div>
+    );
   }
 };
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.autoForm {
+  .form {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .ivu-form-inline .ivu-form-item {
+    margin-right: 0;
+  }
+}
+</style>
