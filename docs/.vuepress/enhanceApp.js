7a8d89bc6928d4ed13facc53a66b130acab845c0
lifeLikeCode <562681041@qq.com>
2019-11-19 13:57:14 +0800

    恢复iview引入

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
-import ViewUI from "view-design";
-import "view-design/dist/styles/iview.css";
+import IView from "iview";
+import "iview/dist/styles/iview.css";
 import axios from "axios";
 import gc_iview from "../../src/index";
 import "./global.css";
 // import App from "./app.vue";
 Vue.prototype.$http = axios;
-Vue.use(ViewUI);
+Vue.use(IView);
 Vue.use(gc_iview);
