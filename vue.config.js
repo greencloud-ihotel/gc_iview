dedc19b5abbe15931f3f3da7a0ce2f1a8d2326e8
lifeLikeCode <562681041@qq.com>
2020-09-03 16:44:08 +0800

    配置lodash按需导入 配置去除外部项等配置

--- vue.config.js
+++ vue.config.js
@@ -10,23 +10,21 @@ module.exports = {
   css: { extract: false },
   productionSourceMap: false,
   configureWebpack: config => {
-    // console.log("NODE_ENV", process.env.NODE_ENV);
-    // if (process.env.NODE_ENV == "production") {
-    config.externals = [
-      {
-        vue: "vue",
-        iview: "iview",
-        axios: "axios",
-        moment: "moment",
-        "animate.css": "animate.css",
-        lodash: "lodash"
-      },
-      /^iview.*/
-    ];
-    // }
-    // console.log("BUID_TYPE", process.env.BUID_TYPE);
-    // if (process.env.BUID_TYPE !== "docs" && config.externals) {
-    //   config.externals.push({ lodash: "lodash" });
-    // }
+    config.externals = {
+      vue: "Vue",
+      "view-design": "iview",
+      axios: "axios",
+      moment: "moment",
+      "animate.css": "animate.css"
+    };
+  },
+  chainWebpack: config => {
+    config.optimization.delete("splitChunks");
+    config.plugins.delete("copy");
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
+    config.plugins.delete("html");
+    config.plugins.delete("hmr");
+    config.entryPoints.delete("app");
   }
 };
